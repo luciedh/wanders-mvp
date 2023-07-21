39c61ba6d8697a6558a49d040beeb96085bf259f
lucie de hauteclocque <lucie.dehauteclocque@gmail.com>
2023-07-21 10:34:47 +0200

    places views

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,12 @@ class PlacesController < ApplicationController
   def index
     if params[:query].present?
       @queries = params[:query].split(";")
-      @searched_places = Place.where(category: @queries)
+      @queries_object = Category.where(name: @queries)
+      @queries_id = []
+      @queries_object.each do |object|
+           @queries_id << object.id
+      end
+      @searched_places = Place.where(category: @queries_id)
     else
       @searched_places = Place.all
     end
@@ -29,9 +34,25 @@ class PlacesController < ApplicationController
     redirect_to place_path(@place)
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update(place_params)
+    redirect_to place_path(@place)
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to places_path
+  end
+
   private
   def place_params
-    params.require(:place).permit(:fr_title, :address, :fr_description)
+    params.require(:place).permit(:fr_title, :address, :fr_description, :img_url, :category_id)
   end
 
 end
--- app/javascript/controllers/category_selection_controller.js
+++ app/javascript/controllers/category_selection_controller.js
@@ -4,6 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["input"]
   selected(event) {
+    console.log(event)
     event.currentTarget.classList.toggle("active")
     this.inputTarget.value += `${event.currentTarget.innerText};`
   }
