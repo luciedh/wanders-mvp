93f35e2ae6f9aa25e3f0071e8bcf4cf9033f79d6
lucie de hauteclocque <lucie.dehauteclocque@gmail.com>
2023-07-29 09:29:18 +0200

    small fixes

--- app/javascript/controllers/display_favourite_description_controller.js
+++ app/javascript/controllers/display_favourite_description_controller.js
@@ -2,8 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="display-favourite-description"
 export default class extends Controller {
-  revealContent(event) {
-    console.log(event)
-    event.currentTarget.classList.toggle("active");
+  static targets = ["description"]
+  revealContent() {
+    this.descriptionTarget.classList.toggle("active");
   }
 }
