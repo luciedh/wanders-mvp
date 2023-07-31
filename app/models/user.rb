a6421eb77c1a690dfbc49789f00e4e336d95bb49
lucie de hauteclocque <lucie.dehauteclocque@gmail.com>
2023-07-31 09:20:37 +0200

    favourite places icone

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :favourite_places, dependent: :destroy
+  has_many :places, through: :favourite_places
 end
