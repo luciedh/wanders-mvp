8c63f343e0f35c2b96e649eedb5ae3604a127502
lucie de hauteclocque <lucie.dehauteclocque@gmail.com>
2023-07-31 10:23:58 +0200

    geoloc user

--- app/javascript/controllers/add_favourite_controller.js
+++ app/javascript/controllers/add_favourite_controller.js
@@ -2,51 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-favourite"
 export default class extends Controller {
-  connect() {
-    console.log("Hello from add favourite controller")
-  }
-
-  addFavourite(data){
-    fetch('/favourite_places', {
-      method: "POST",
-      headers: {'Accept': 'application/json',
-                'X-CSRF-Token': document.querySelector('[name="csrf-token"]').content
-               },
-      body: data
-    })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data)
-      })
-  }
-
-  removeFavourite(data, id){
-    fetch(`/favourite_places/${id}`, {
-      method: "DELETE",
-      headers: {'Accept': 'application/json',
-                'X-CSRF-Token': document.querySelector('[name="csrf-token"]').content
-               },
-      body: data
-    })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data)
-      })
-  }
 
   changeIcon(event){
     event.preventDefault();
-    const data = new FormData();
-    let id = '';
-    id = this.iconTarget.dataset.favid;
-    data.append('favourite_place[place_id]', this.iconTarget.dataset.place)
-    data.append('favourite_place[user_id]', this.iconTarget.dataset.user)
+    console.log(event);
     if (this.iconTarget.classList.contains('fa-regular')) {
       this.iconTarget.classList.replace('fa-regular', 'fa-solid')
-      this.addFavourite(data)
     } else {
       this.iconTarget.classList.replace('fa-solid', 'fa-regular')
-      this.removeFavourite(data, id)
     }
   }
 }
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -26,12 +26,94 @@ export default class extends Controller {
           .setPopup(popup)
           .addTo(this.map)
       })
+      this.#handleUserLocation()
     }
 
     #fitMapToMarkers() {
       const bounds = new mapboxgl.LngLatBounds()
       this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
       this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    }
+
+    #handleUserLocation() {
+      const previousUserCoords = this.#getUserCoordsFromLocalStorage()
+      if (previousUserCoords) {
+        this.#centerMapToCoords(previousUserCoords[0], previousUserCoords[1])
+        this.#createUserMarker(previousUserCoords[0], previousUserCoords[1])
+      }
+      this.#getCurrentPosition()
+    }
+
+    #setUserCoordsInLocalStorage(lat, lng) {
+      localStorage.setItem('userCoords', `[${lat}, ${lng}]`)
+    }
+
+    #getUserCoordsFromLocalStorage() {
+      try {
+        return JSON.parse(localStorage.getItem('userCoords'))
+      } catch (error) {
+        console.warn(error);
+      }
+    }
+
+    disconnect() {
+      this.#setUserCoordsInLocalStorage(this.userLat, this.userLng)
+    }
+
+    #getCurrentPosition() {
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 1000,
+        maximumAge: 0,
+      }
+
+      const success = (pos) => {
+        const crd = pos.coords
+        this.userLat = crd.latitude
+        this.userLng = crd.longitude
+        this.#setUserCoordsInLocalStorage(crd.latitude, crd.longitude)
+        this.#centerMapToCoords(crd.latitude, crd.longitude)
+        console.log([this.userLat, this.userLng]);
+      }
+
+      const error = (err) => {
+        console.warn(`ERROR(${err.code}): ${err.message}`)
+      }
+
+      navigator.geolocation.getCurrentPosition(success.bind(this), error.bind(this), options);
+    }
+
+    #centerMapToCoords(lat, lng) {
+      const bounds = new mapboxgl.LngLatBounds()
+      bounds.extend([ lng, lat ])
+      this.map.fitBounds(bounds, this.#fitOptions())
+    }
+
+    #fitOptions() {
+      return { padding: 70, maxZoom: 15, duration: 0 }
+    }
+
+    #getUserLocation(position) {
+      if (this.userMarker) this.userMarker.remove()
+
+      const lat = position.coords.latitude
+      const lng = position.coords.longitude
+      this.userLat = lat
+      this.userLng = lng
+      this.#setUserCoordsInLocalStorage(lat, lng)
+      this.#createUserMarker(lat, lng)
+    }
+
+    #createUserMarker(lat, lng) {
+      const el  = document.createElement('div')
+
+      el.classList = "user-marker"
+      el.innerHTML = '<i class="fa-solid fa-person" id="user-icon"></i>';
+
+      this.userMarker = new mapboxgl.Marker(el)
+        .setLngLat([lng, lat])
+        .addTo(this.map)
+      this.#centerMapToCoords(lat, lng)
     }
 
   }
